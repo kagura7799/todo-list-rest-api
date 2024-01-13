d079869684c1b96a5598d43453892c5aec7ccb5b
maksim <mkotelnikov97@gmail.com>
2024-01-13 20:23:28 +0300

    base feat: Функционал просмотра задач

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"todo/internal/app"
+	"todo/internal/db"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
-	myApp := app.App{}
+	database, err := db.NewDB()
+
+	if err != nil {
+		panic(err)
+	}
+
+	myApp := app.App{
+		DB: database,
+	}
+
 	r.POST("/todoList", myApp.HomeHandler)
 	r.POST("/todoList/add", myApp.NewTask)
 	r.POST("/todoList/delete", myApp.DeleteTask)
+	r.POST("/todoList/get", myApp.ReadTasks)
 
 	r.Run(":8080")
 }
